531ae819c7e3da4b6741f1f1c4aa93394ee0d8e5
maestroDever <dream.big.dev.423@gmail.com>
2020-04-21 22:50:41 +0300

    add search box

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,36 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <router-link to="/">
+          <v-img
+            alt="Logo"
+            class="shrink mr-2"
+            contain
+            src="https://via.placeholder.com/150"
+            transition="scale-transition"
+            width="40"
+            rounded
+          />
+        </router-link>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-autocomplete
+        v-model="select"
+        :loading="loading"
+        :items="items"
+        :search-input.sync="search"
+        cache-items
+        class="mx-4"
+        flat
+        hide-no-data
+        hide-details
+        label="Search"
+        solo-inverted
+        rounded
+        prepend-inner-icon="mdi-magnify"
+      ></v-autocomplete>
     </v-app-bar>
 
     <v-content> </v-content>
@@ -42,7 +43,29 @@ export default {
   name: "App",
 
   data: () => ({
-    //
-  })
+    loading: false,
+    items: [],
+    search: null,
+    select: null,
+    states: ["Alabama", "Alaska", "American Samoa"]
+  }),
+
+  watch: {
+    search(val) {
+      val && val !== this.select && this.querySelections(val);
+    }
+  },
+  methods: {
+    querySelections(v) {
+      this.loading = true;
+      // Simulated ajax query
+      setTimeout(() => {
+        this.items = this.states.filter(e => {
+          return (e || "").toLowerCase().indexOf((v || "").toLowerCase()) > -1;
+        });
+        this.loading = false;
+      }, 500);
+    }
+  }
 };
 </script>
