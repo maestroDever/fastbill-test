f2a5b7a72b67bc04f1374c04ebdf04f7e0925315
maestroDever <dream.big.dev.423@gmail.com>
2020-04-21 22:13:14 +0300

    remove helloworld

--- src/App.vue
+++ src/App.vue
@@ -33,21 +33,13 @@
       </v-btn>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld />
-    </v-content>
+    <v-content> </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
 
   data: () => ({
     //
